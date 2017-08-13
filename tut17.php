b4a14f0d6bf5c632f832a89b50a43b392d7b2c6c
disha98 <dishajanardhan@hotmail.com>
2017-08-13 18:27:59 +0530

    Create tut17.php

---
+++ tut17.php
@@ -0,0 +1,27 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+<style>@import url('https://fonts.googleapis.com/css?family=Pacifico'); </style>
+	<title> Registeration </title>
+	<link rel="stylesheet" type="text/css" href="formstyle.css">
+</head>
+<body>
+<div id="frm" class="container">
+<form action="process.php" method="POST">
+	<h1><br>Tech Tatva '17</h1>
+	<p>
+	<label></label>
+	<input type="text" id="username" name="username" placeholder="Enter username"/>
+	</p>
+	<p>
+	<label> </label>
+	<input type="password" id="passw" name="passw" placeholder="Enter password"></input>
+	</p>
+	<p>
+	<input type="submit" id="btn" value="LOGIN" class="btn-login"></input>
+	</p>
+</form>
+</div>
+</body>
+</html>
